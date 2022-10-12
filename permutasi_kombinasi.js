fa88730da10ed726dcb7fe673adfede8d7993ece
M Hashfi Fahlevi <91356942+brostudio000@users.noreply.github.com>
2022-10-12 07:08:04 +0700

    Update permutasi_kombinasi
    Tambah fungsi hitung kombinasi

--- permutasi_kombinasi.js
+++ permutasi_kombinasi.js
@@ -2,12 +2,16 @@
 const n = 5;
 const r = 3;
 
+console.log('Permutasi');
+console.log('Nilai n:',n);
+console.log('Nilai r:',r);
+
 function nilaiN() {
     let factN = 1;
     for (i = 1; i <= n; i++) {
         faktorialN = factN *= i;
     }
-    console.log('Faktorial dari',n,'=',faktorialN)
+    console.log('Faktorial dari n','=',faktorialN)
 }
 
 function nilaiX() {
@@ -16,11 +20,38 @@ function nilaiX() {
     for (i = 1; i <= x; i++) {
         faktorialX = factX *= i;
     }
-    console.log('Faktorial dari',n,'-',r,'=',faktorialX)
+    console.log('Faktorial dari n-r','=',faktorialX)
 }
+
 nilaiN();
 nilaiX();
 
-console.log('Permutasi',r,'dari',n,'adalah:',(faktorialN / faktorialX));
+console.log('Permutasi',r,'dari',n,'objek yang berbeda adalah:',(faktorialN / faktorialX));
+console.log('\n')
 
-// Kombinasi (otw maszeh hehe)
+const k = 2;
+console.log('Kombinasi')
+console.log('Nilai n:',n);
+console.log('Nilai k:',k);
+
+function nilaiK() {
+    let factK = 1;
+    for (i = 1; i <= k; i++) {
+        faktorialK = factK *= i;
+    }
+    console.log('Faktorial dari k adalah:',faktorialK);
+}
+
+function nilaiY() {
+    const y = n-k;
+    let factY = 1;
+    for (i = 1; i <= y; i++) {
+        faktorialY = factY *= i;
+    }
+    console.log('Faktorial dari n-k adalah:',faktorialY);
+}
+nilaiK();
+nilaiY();
+
+comb = (faktorialN) / (faktorialK * faktorialY);
+console.log('Kombinasi',k,'dari',n,'objek yang berbeda adalah:',comb);
